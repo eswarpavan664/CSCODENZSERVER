413b5ea37edd64536a5b5f63918ab660bd79b82b
pavaneswar kothalanka <itonystark58@gmail.com>
2022-12-02 21:42:59 +0530

    first commit

--- keys.js
+++ keys.js
@@ -1,7 +1,5 @@
-module.exports={
-    mogoUrl: "mongodb+srv://Eswar0007:Result2023@cluster0.rpzbmu0.mongodb.net/?retryWrites=true&w=majority",
-    jwtkey:"jsdfgsdjgfsf"
-}
-
-
- 
+module.exports = {
+    mogoUrl:
+        "mongodb+srv://Eswar0007:Result2023@cluster0.rpzbmu0.mongodb.net/?retryWrites=true&w=majority",
+    jwtkey: "jsdfgsdjgfsf",
+};
--- server.js
+++ server.js
@@ -1,63 +1,47 @@
-const express = require('express');
- 
+const express = require("express");
+
 const app = express();
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-const fast2sms = require('fast-two-sms')
-const {mogoUrl} = require('./keys');
-mongoose.connect(mogoUrl)
- 
-require('./models');
- 
-var nodemailer = require('nodemailer');
+const fast2sms = require("fast-two-sms");
+const { mogoUrl } = require("./keys");
+mongoose.connect(mogoUrl);
+
+require("./models");
+
+var nodemailer = require("nodemailer");
 
 var transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
-    user: 'cscodenzofficial20@gmail.com',
-    pass: 'tlfpgphzisjeslbv'
-  }
+    user: "cscodenzofficial20@gmail.com",
+    pass: "tlfpgphzisjeslbv",
+  },
 });
 
- 
-
- 
-
-const authRoutes = require('./authRouters');
+const authRoutes = require("./authRouters");
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(authRoutes)
- 
+app.use(authRoutes);
 
-mongoose.connect(mogoUrl,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-mongoose.connection.on('connected',()=>{
-    console.log("database connected ...")
-})
+mongoose.connect(mogoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("database connected ...");
+});
 
+mongoose.connection.on("error", (err) => {
+  console.log("error occered... ", err);
+});
 
-mongoose.connection.on('error',(err)=>{
-    console.log("error occered... ",err);
-})
-
- 
-
- 
-
-
-app.get('/',(req,res)=>{
-    res.send("Working");
-
-})
-
-
-
-
+app.get("/", (req, res) => {
+  res.send("Working");
+});
 
 /*
 
@@ -65,56 +49,47 @@ zjzacbjjnvmbhtzh
 
 */
 
+app.get("/sendOrderAsSms", async (req, res) => {
+  const orderid = req.query.OrderId;
+  const customername = req.query.CustomerName;
+  const phonenumber = req.query.PhoneNumber;
+  var options = {
+    authorization:
+      "ul5V2dpBCiEz01rOck739GoyHKIZfmbwXhtnevLW4JFDMRUq8YFLZeUPJW23qOaY5R8CjcVsTE6rxIzG",
+    message:
+      "Customer:- " +
+      customername +
+      "\t Mobile No:-" +
+      phonenumber +
+      " with Order Id:-" +
+      orderid,
+    numbers: ["7993031882"],
+  };
+  const response = await fast2sms.sendMessage(options);
 
+  res.send(response);
+  // console.log(orderid,customername,phonenumber);
+});
 
- 
+app.get("/sendgmail", (req, res) => {
+  const msg = req.query.msg;
+  const gmail = req.query.gmail;
 
-app.get('/sendOrderAsSms',async (req,res)=>{
-    const orderid = req.query.OrderId ;
-    const customername = req.query.CustomerName ;
-    const phonenumber = req.query.PhoneNumber ;
-   var options = {authorization :'ul5V2dpBCiEz01rOck739GoyHKIZfmbwXhtnevLW4JFDMRUq8YFLZeUPJW23qOaY5R8CjcVsTE6rxIzG' , message : 'Customer:- '+customername+'\t Mobile No:-'+phonenumber+' with Order Id:-'+orderid ,  numbers : ['7993031882']} 
-    const response = await fast2sms.sendMessage(options)
- 
-     res.send(response)
-    // console.log(orderid,customername,phonenumber);
-    
+  var mailOptions = {
+    from: "cscodenzofficial20@gmail.com",
+    to: gmail,
+    subject: "CS CODENZ Educational Society",
+    text: `${msg}Website URL:www.cscodenz.org`,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.send({ status: "No" });
+    } else {
+      res.send({ status: "Done" });
+    }
+  });
+});
 
-})
-
-
-
-
-app.get('/sendgmail',(req,res)=>{
-
-    const msg=req.query.msg;
-    const gmail =req.query.gmail;
-
-    var mailOptions = {
-        from: 'cscodenzofficial20@gmail.com',
-        to: gmail,
-        subject: 'CS CODENZ Educational Society',
-        text:`
-            ${msg}
-
-
-            
-        Website URL:www.cscodenz.org
-        `
-      };
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            res.send({"status":"No"});
-        } else {
-          res.send({"status":"Done"});
-        }
-      });
-       
-})
- 
- 
- 
-
-app.listen(process.env.PORT || 5000,()=>{
-    console.log("server is runnung on port 5000");
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server is runnung on port 5000");
+});
